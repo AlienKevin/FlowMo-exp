c353fffc13de2d2a807606ab7f78e37c968efc39
Kevin Li <kevinli020508@gmail.com>
2025-06-27 08:53:06 -0700

    Backprop decoder losses through prior but stop prior loss to encoder

--- train_larp.sh
+++ train_larp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=viscam
 #SBATCH --partition=viscam
-#SBATCH --gres=gpu:l40s:4
+#SBATCH --gres=gpu:h200:4
 #SBATCH --time=2880
 #SBATCH --cpus-per-task=64
 #SBATCH --job-name=rand
@@ -22,21 +22,20 @@ MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
 echo "Using MASTER_PORT="$MASTER_PORT
 
 torchrun --nproc-per-node=4 --master_port=$MASTER_PORT -m flowmo.train \
-    --experiment-name "dogs_flowmo_lo_c2i_larp_ibq_rand_128x128_pretrain" \
-    data.imagenet_train_index="dogs_imagenet_train_index.json" \
+    --experiment-name "e2e_h200_flowmo_lo_c2i_ibq_rand_128x128_pretrain" \
     model.context_dim=18 model.codebook_size_for_entropy=9 model.quantization_type="larp_ibq" \
     model.patch_size=8 model.mup_width=4 model.code_length=128 \
     prior.model_name="Qwen3-0.6B" \
     prior.random_init=True \
     prior.stop_grad=False \
-    prior.loss_weight=0.001 \
+    prior.loss_weight=1.0 \
     prior.lr_multiplier=1 \
     data.batch_size=64 \
     data.eval_batch_size=40 \
     data.image_size=128 \
-    opt.n_grad_acc=2 \
+    opt.n_grad_acc=4 \
     opt.lr=0.0001 \
-    opt.freeze_encoder_after=50000 \
+    opt.freeze_encoder_after=9999999999 \
     trainer.max_steps=200000 \
     trainer.checkpoint_every=50000 \
     trainer.keep_every=50000
